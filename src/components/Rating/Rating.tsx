f0f8c80565e0e5b820e8854d07b82d7e6c140d9e
Bogdan <b@bmazur.ru>
2022-11-19 01:31:12 +0300

    fix: key

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,32 +1,38 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-return-assign */
-import React from 'react';
+import React, { useState } from 'react';
 
 import Star from './Star';
 import mockData from './mockData';
 
+interface IStar {
+  key?: string,
+  clicked: boolean,
+  active: boolean,
+}
+
 export default function Rating() {
-  const [stars, setStars] = React.useState(mockData);
+  const [stars, setStars] = useState<IStar[]>(mockData);
 
   const handleClick = (id: number) => {
-    const i = stars.reduce((count: number, item: Record<string, boolean>) => (item.clicked
+    const i = stars.reduce((count: number, item: IStar) => (item.clicked
       ? count += 1 : count), 0);
     const val = (id - i >= -1) ? !stars[id].clicked : true;
-    const arr = stars.map(({ active }, index) => (id >= index
+    const arr: IStar[] = stars.map(({ active }, index) => (id >= index
       ? { clicked: val, active } : { clicked: false, active }));
 
     setStars(arr);
   };
 
   const handleFocus = (id: number) => {
-    const arr = stars.map(({ clicked }, index) => (id >= index
+    const arr: IStar[] = stars.map(({ clicked }, index) => (id >= index
       ? { clicked, active: true } : { clicked, active: false }));
 
     setStars(arr);
   };
 
   const handleUnFocus = () => {
-    const arr = stars.map(({ clicked }) => ({ clicked, active: clicked }));
+    const arr: IStar[] = stars.map(({ clicked }) => ({ clicked, active: clicked }));
 
     setStars(arr);
   };
@@ -35,7 +41,7 @@ export default function Rating() {
     <div className="rating">
       {stars.map((star, index) => (
         <Star
-          key={index}
+          key={star.key}
           id={index}
           active={star.active}
           handleClick={handleClick}
--- src/components/Rating/mockData.ts
+++ src/components/Rating/mockData.ts
@@ -1,21 +1,26 @@
 const mockData = [
   {
+    key: '1',
     clicked: false,
     active: false,
   },
   {
+    key: '2',
     clicked: false,
     active: false,
   },
   {
+    key: '3',
     clicked: false,
     active: false,
   },
   {
+    key: '4',
     clicked: false,
     active: false,
   },
   {
+    key: '5',
     clicked: false,
     active: false,
   },
