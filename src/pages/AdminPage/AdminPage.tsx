ac0e51dbb212120c592275cd5c2fa60fd6dd7e70
Bogdan <b@bmazur.ru>
2022-11-19 18:40:53 +0300

    feat: admin-page

---
+++ src/pages/AdminPage/AdminPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Admin from './Admin';
+import withUser from '../../hoc/withUser';
+import Content from '../../components/Content';
+
+function AboutPage() {
+  return (
+    <Content heading="Admin" header menu>
+      <Admin />
+    </Content>
+  );
+}
+
+export default withUser(AboutPage, false);
