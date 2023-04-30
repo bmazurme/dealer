ac6ba7695bf54135e98fe3768b26136b4fe48eb8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-04-30 12:39:52 +0300

    fix: css-module

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import style from './chip.module.css';
+
 export default function Chip({ value, type } : { value: string, type?: string }) {
   return (
-    <div className={`chip ${type === 'outline' && 'chip_outline'}`}>
-      <span className="chip__label">
+    <div className={`${style.chip} ${type === 'outline' && style.chip_outline}`}>
+      <span className={style.chip__label}>
         { value }
       </span>
     </div>
