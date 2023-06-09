60c8ec5af9d713e247f7afc5328f8ccf7f92e706
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-09 22:47:29 +0300

    fix: error-boundary

--- src/components/error-boundary-wrapper/error-boundary-wrapper.tsx
+++ src/components/error-boundary-wrapper/error-boundary-wrapper.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-
 import type { PropsWithChildren } from 'react';
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import Header from '../header';
 import Button from '../button';
 import Footer from '../footer';
 import Banner from '../banner';
+
+import style from './error-boundary-wrapper.module.css';
 
 type ErrorBoundaryWrapperProps = PropsWithChildren<unknown>;
 
@@ -16,39 +18,41 @@ function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
     <>
       <Header />
-
-      <div className="error-boundary">
-        <div className="error-boundary__title">
-          <h2 className="error-boundary__title">APP-ERROR </h2>
-          <p className="error-boundary__message">{error.message}</p>
-
-          <div className="error-boundary__block">
+      <div className={style['error-boundary']}>
+        <div className={style['error-boundary__title']}>
+          <h2 className={style['error-boundary__title']}>APP-ERROR </h2>
+          <p className={style['error-boundary__message']}>{error.message}</p>
+          <div className={style['error-boundary__block']}>
             Try to
-            <Button className="button button_reload" onClick={resetErrorBoundary} variant="outline">
+            <Button
+              className={classnames('button', 'button_reload')}
+              onClick={resetErrorBoundary}
+              variant="outline"
+            >
               Reload app
             </Button>
             or
-            <Button className="link link_home" onClick={resetErrorBoundary} variant="outline" as={Link} to="/">
+            <Button
+              className={classnames('link', 'link_home')}
+              onClick={resetErrorBoundary}
+              variant="outline"
+              as={Link}
+              to="/"
+            >
               Go to homepage
             </Button>
           </div>
         </div>
       </div>
-
       <Footer />
       <Banner />
     </>
   );
 }
 
-export default function ErrorBoundaryWrapper({
-  children,
-}: ErrorBoundaryWrapperProps) {
+export default function ErrorBoundaryWrapper({ children }: ErrorBoundaryWrapperProps) {
   return (
-    <ErrorBoundary
-      onReset={() => {}}
-      FallbackComponent={ErrorFallback}
-    >
+    <ErrorBoundary onReset={() => {}} FallbackComponent={ErrorFallback}>
       {children}
     </ErrorBoundary>
   );
