921cc138337ed86c4eff6ffcb92c5d77bbea9676
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-10 11:46:51 +0300

    fix: icon button

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
+import classnames from 'classnames';
+
+import style from './icon-button.module.css';
 
 type IconButtonProps<T extends ElementType> = PropsWithChildren<{
   as?: T;
   className?: string;
   variant: 'filled' | 'outline' | 'link' | 'icon';
   disabled?: boolean;
+  htmlType?: string;
 }> & Omit<ComponentPropsWithoutRef<T>, 'as' | 'variant' | 'children'>;
 
 export default function IconButton<T extends ElementType = 'button'>({
-  as, className, disabled, ...props
+  as, className, disabled, htmlType, ...props
 }: IconButtonProps<T>) {
   const Component = as ?? 'button';
 
   return (
     <Component
-      className={`${className} ${disabled ? 'icon-button_disabled' : ''}`}
+      className={classnames(className, { [style['icon-button_disabled']]: disabled })}
       disabled={disabled}
+      type={htmlType ? 'submit' : 'button'}
       {...props}
     />
   );
